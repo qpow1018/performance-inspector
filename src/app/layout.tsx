913bfd01ee84382c524e1648105d6985fdb980d9
WonjinChoi <qpow1018@gmail.com>
2026-08-27 11:41:22 +0900

    chore: migrate to next.js app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+type RootLayoutProps = Readonly<{
+  children: React.ReactNode
+}>
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  )
+}
